6261aa12de4b2be3feb358379a287c0741103583
prashant-m4 <prashantsingh090798@gmail.com>
2025-06-22 19:21:32 +0530

    feat: added routes

---
+++ app/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get('/api', (_req, res) => {
+    res.send('Hello, world!');
+});
+
+app.post('/api/:id', (req, res) => {
+    res.send('POST request received with id: ' + req.params.id);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
