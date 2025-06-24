558763d4a9657ce025d1a7d7db4cc98e16cde9a9
prashant-m4 <prashantsingh090798@gmail.com>
2025-06-24 21:49:07 +0530

    feat: update amazon-linux with ubuntu in terraform

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 (for us-east-1)
+  ami           = "ami-0fc5d935ebf8bc3bc"  # Amazon Linux 2 (for us-east-1)
   instance_type = "t2.micro"              # Free-tier eligible
   key_name      = aws_key_pair.m4_key.key_name
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
